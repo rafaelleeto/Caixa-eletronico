4b9c16e2d06b2355980b6d475b4a4b3732b34783
rafaelleeto <leetobr@gmail.com>
2024-09-12 14:37:05 -0300

    ola

--- C/caixa.c
+++ C/caixa.c
@@ -1,60 +1,61 @@
-#include <stdio.h>
-    int senha,opcao,agencia;
-    float saldo=0.0,saque=0.0,deposito=0.0,limite=500.0;
+int senha, opcao, agencia;
+float saldo = 0.0, saque = 0.0, deposito = 0.0;
 
+int main() {
+    // Solicita a senha
+    printf("Bem-vindo ao caixa eletrônico. Digite sua senha: ");
+    scanf("%d", &senha);
 
-int main(){
-  
-
-
-    printf("Bem vindo ao caixa eletronico, digite sua senha: ");
-    scanf("%d",&senha);
-    do{
-    if (senha!=2805){
-        printf("Senha inválida, tente novamente: ");
-        scanf("%d",&senha);
-    }} while (senha!=2805);
-
-
-    printf("Informe a sua agencia: ");
-    scanf("%d",&agencia);
-    do{
-        printf("Agencia errada, digite novamente: \n");
-        scanf("%d",&agencia);
-        } 
-    while (agencia!=4020);
-
-    
-
-
-    printf("Escolha uma opcao: \n1- saldo \n2-extrato \n3-saque \n4-deposito\n ");
-    scanf("%d",&opcao);
-    do{
-    if (opcao!=1 && opcao!=2 && opcao!=3 && opcao!=4 ){
-        printf("opcao invalida, tente novamente:\n ");
-        printf("Escolha uma opcao: \n1- saldo \n2-extrato \n3-saque \n4-deposito\n ");
-        scanf("%d",&opcao);
-    }} while (opcao!=1 && opcao!=2 && opcao!=3 && opcao!=4);
-
-    switch (opcao)
-    {
-    case 1:
-        printf("Você tem um saldo total de : %2.f",saldo);
-        break;
-    case 2:
-        printf("extrato : %2.f",saldo);
-        break;
-    case 3:
-        printf("Informe um valor para sacar : ");
-        scanf("%f",&saque);
-        saldo=saldo-saque;
-        break;
-    case 4:
-        printf("Informe um valor para depositar : ");
-        scanf("%f",&deposito);
-        saldo=deposito+saldo;
-        break;
-            
+    while (senha != 2805) {
+        printf("Senha inválida. Tente novamente: ");
+        scanf("%d", &senha);
     }
-return 0;
+
+    // Solicita a agência
+    printf("Informe a sua agência: ");
+    scanf("%d", &agencia);
+
+    while (agencia != 4020) {
+        printf("Agência inválida. Tente novamente: ");
+        scanf("%d", &agencia);
+    }
+
+    // Menu de opções
+    do {
+        printf("Escolha uma opção:\n1 - Saldo\n2 - Extrato\n3 - Saque\n4 - Depósito\n5- Sair");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                printf("Você tem um saldo total de: %.2f\n", saldo);
+                break;
+            case 2:
+                printf("Extrato: %.2f\n", saldo);
+                break;
+            case 3:
+                printf("Informe um valor para sacar: ");
+                scanf("%f", &saque);
+                if (saque > saldo) {
+                    printf("Você não possui essa quantia de dinheiro. Você possui um total de %.2f!\n", saldo);
+                } else {
+                    saldo -= saque;
+                    printf("Saque de %.2f realizado com sucesso.\n", saque);
+                }
+                break;
+            case 4:
+                printf("Informe um valor para depositar: ");
+                scanf("%f", &deposito);
+                saldo += deposito;
+                printf("Depósito de %.2f realizado com sucesso.\n", deposito);
+                break;
+            case 5: 
+                return 2;
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+                break;
+        }
+    } while (opcao < 1 || opcao > 4);
+
+    return 0;
 }
